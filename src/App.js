3b2332b342a292967a582d3d16b4f9da5995ce26
Harinder daksh <81361432+harrydaksh@users.noreply.github.com>
2023-08-28 15:16:38 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    knkkj
-    </div>
-  );
+    return (
+        <div className="App">
+            Hi, All
+            This is my new profile
+            <h1>Comming soon</h1>
+        </div>
+    );
 }
 
 export default App;
